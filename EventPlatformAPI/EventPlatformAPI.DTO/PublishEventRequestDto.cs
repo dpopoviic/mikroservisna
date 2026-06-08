bf9d4911c15e67b3fb300792e4d3ae6e169a5b54
Dragana Popović <dp20220322@student.fon.bg.ac.rs>
2026-06-09 00:35:52 +0200

    Kreirane DTO klase PublishRequestDto i PublishEventResultDto

---
+++ EventPlatformAPI/EventPlatformAPI.DTO/PublishEventRequestDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventPlatformAPI.DTO
+{
+    public class PublishEventRequestDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Agenda { get; set; } = string.Empty;
+        public DateTime DateTime { get; set; }
+        public decimal DurationInHours { get; set; }
+        public decimal Price { get; set; }
+        public int TypeId { get; set; }
+        public int LocationId { get; set; }
+        public List<int> LecturerIds { get; set; } = [];
+        public string OrganizerEmail { get; set; } = string.Empty;
+    }
+}
---
+++ EventPlatformAPI/EventPlatformAPI.DTO/PublishEventResultDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventPlatformAPI.DTO
+{
+    public class PublishEventResultDto
+    {
+        public Guid CorrelationId { get; set; }
+        public string Message { get; set; } = string.Empty;
+    }
+}
