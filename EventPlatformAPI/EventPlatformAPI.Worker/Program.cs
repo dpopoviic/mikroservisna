0d1f4d201f1ff6907445215e5b428ea797cb350f
Dragana Popović <dp20220322@student.fon.bg.ac.rs>
2026-06-17 01:29:56 +0200

    Implementiran saga orkestrator patern u eservisu ya slanje mejlova

--- EventPlatformAPI/EventPlatformAPI.Worker/Program.cs
+++ EventPlatformAPI/EventPlatformAPI.Worker/Program.cs
@@ -8,7 +8,8 @@ builder.ConfigureServices((context, services) =>
     services.Configure<RabbitMqOptions>(context.Configuration.GetSection("RabbitMq"));
     services.Configure<WorkerOptions>(context.Configuration.GetSection("Worker"));
 
-    services.AddHostedService<EmailQueueConsumerHostedService>();
+   services.AddHostedService<EmailQueueConsumerHostedService>();
+   services.AddHostedService<SagaEmailCommandConsumerHostedService>();
 });
 
 var host = builder.Build();
